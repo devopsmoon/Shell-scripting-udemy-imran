1ae57f816387a6312c94a1d3d465f24b91524628
devopsmoon <md_syd01@yahoo.co.in>
2021-04-11 19:41:21 +0530

    basic command script

---
+++ basic-command script.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+# Author: Mohammed sayeeduddin
+# Date: 10-04-2021
+# Description: This script will give you the basic task
+# Modified Date: 10-04-2021
+echo
+echo "This script will give the Basci command output"
+echo
+pwd
+echo
+date
+echo
+touch sayeed rayan rayan
+echo
+ll -lart
+echo
+calc
+echo
+whoami
+echo
+echo $0
+echo
+echo $SHELL
+echo
+cat /etc/shells
+
