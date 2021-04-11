95dfd70c49ca62377fc6301bd4aafb9d2ad84754
devopsmoon <md_syd01@yahoo.co.in>
2021-04-11 20:07:43 +0530

    variable-test

---
+++ varaiable.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Author: mohammed sayeeduddin
+# Date: 10-04-2021
+# Description: this script will print variable data
+# Modified Date: 10-04-2021
+echo -n "This script will print the variables"
+p=pwd
+l=ls
+wi=whoami
+d=date
+c='cal 2021'
+echo $p
+echo $l
+echo $wi
+echo $c
